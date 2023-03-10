c987ef53969aab57b40735610eadb4f578c2fe75
Patrick van der Flier <patrick@corverdevelopment.nl>
2023-03-10 17:59:08 +0100

    Remove chat gpt nav bar, that was shit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
     </div>
   );
 }
